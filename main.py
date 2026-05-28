568ed72b0ae6db6d4e43260c86874f650aeff67f
Rusty <arust23@gmail.com>
2026-05-27 18:02:36 -0600

    Refactor birthday email notification logic
    Refactor birthday email notification system to use pandas for reading CSV and streamline email sending process.

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
+import pandas as pd
 import smtplib
+import random
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+today = dt.datetime.today()
+birthdays_today = []
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+df = pd.read_csv("birthdays.csv")
+df.columns = df.columns.str.strip()
+    #clean column names
+df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+for index, row in df.iterrows():
+    name = row["name"]
+    email = row["email"]
+    year = row["year"]
+    month = int(row["month"])
+    day = int(row["day"])
+    if month == today.month and day == today.day:
+        birthdays_today.append({"name": name, "email": email})
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+letter_templates = [
+    "letter_templates/letter_1.txt",
+    "letter_templates/letter_2.txt",
+    "letter_templates/letter_3.txt",
+]
+
+for birthday in birthdays_today:
+    with open(random.choice(letter_templates), "r") as f:
+        letter = f.read()
+    name = birthday["name"]
+    email = birthday["email"]
+    personalized = letter.replace("[NAME]", name)
+
+    user_email=os.environ.get("user_email)
+    password = os.environ.get("password")
+
+    connection = smtplib.SMTP("smtp.gmail.com", 587)
+    connection.starttls()
+    connection.login(user_email, password)
+    connection.sendmail(
+        from_addr=user_email,
+        to_addrs=email, msg= f"Subject: Happy Birthday!\n\n {personalized}"
+    )
+    connection.quit()
